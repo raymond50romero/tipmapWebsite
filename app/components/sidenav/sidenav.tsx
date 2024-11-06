69d12aa6c2caf72d9d66513b8c5b754af621d47a
Raymond Romero <raymond50romero@gmail.com>
2024-11-05 21:31:21 -0800

    pushing updates

--- app/components/sidenav/sidenav.tsx
+++ app/components/sidenav/sidenav.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 export default function SideNav() {
   return (
-    <div className="h-full w-full border border-t-0">
-      <h1>side nav</h1>
+    <div className="h-full w-full border border-t-0 flex flex-col justify-evenly">
+      <section id="side-top-navigation" className="border-b-[1px] h-full">
+        navigate
+      </section>
+      <section id="side-middle-navigation" className="border-b-[1px] h-full">
+        user stuff
+      </section>
+      <section id="side-bottom-navigation" className="border-b-[1px] h-full">
+        general
+      </section>
     </div>
   );
 }
