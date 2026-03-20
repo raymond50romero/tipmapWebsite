afbb8cb9e5c7abb865e024f1e37802682e82120d
Raymond Romero <raymond50romero@gmail.com>
2026-03-20 08:23:59 -0700

    added dollar ratings for weekend and weekday tips

--- src/features/posts/dollarRating.jsx
+++ src/features/posts/dollarRating.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 import "./styles.css";
 
-export default function StarRating({ rating }) {
+export default function DollarRating({ rating }) {
   const baseId = useId();
 
   return (
@@ -22,12 +22,13 @@ export default function StarRating({ rating }) {
           >
             <defs>
               <linearGradient id={gradId}>
-                <stop offset={`${fillPercentage}%`} stopColor="#fbbf24" />
+                <stop offset={`${fillPercentage}%`} stopColor="#22c55e" />
                 <stop offset={`${fillPercentage}%`} stopColor="#ccc" />
               </linearGradient>
             </defs>
+            {/* Dollar Sign Path */}
             <path
-              d="M12 2l2.9 6 6.6.9-4.8 4.7 1.1 6.6L12 17.8l-5.8 3 1.1-6.6-4.8-4.7 6.6-.9z"
+              d="M11.8 10.9c-2.27-.59-3-1.2-3-2.15 0-1.09 1.01-1.85 2.7-1.85 1.78 0 2.44.85 2.5 2.1h2.21c-.07-1.72-1.12-3.3-3.21-3.81V3h-3v2.16c-1.94.42-3.5 1.68-3.5 3.61 0 2.31 1.91 3.46 4.7 4.13 2.5.6 3 1.48 3 2.41 0 .69-.49 1.79-2.7 1.79-2.06 0-2.87-.92-2.98-2.1h-2.2c.12 2.19 1.76 3.42 3.68 3.83V21h3v-2.15c1.95-.37 3.5-1.5 3.5-3.55 0-2.84-2.43-3.81-4.7-4.4z"
               fill={`url(#${gradId})`}
             />
           </svg>
@@ -37,6 +38,6 @@ export default function StarRating({ rating }) {
   );
 }
 
-StarRating.propTypes = {
+DollarRating.propTypes = {
   rating: PropTypes.number,
 };
--- src/features/posts/eachPost.jsx
+++ src/features/posts/eachPost.jsx
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import StarRating from "./starRating.jsx";
+import DollarRating from "./dollarRating.jsx";
 
 import "./styles.css";
 
@@ -10,10 +11,22 @@ export default function EachPost({ post }) {
     <div className="each-post-container">
       <h6 className="each-post-header">{post.restaurant_name}</h6>
       <p className="each-post-title">{post.title}</p>
-      <div className="each-post-rating">
-        <span>Clientele:</span>
-        <StarRating rating={post.clientele} />
+
+      <div className="ratings-group">
+        <div className="each-post-rating">
+          <span className="rating-label">Weekday Tips:</span>
+          <DollarRating rating={post.weekday_tips} />
+        </div>
+        <div className="each-post-rating">
+          <span className="rating-label">Weekend Tips:</span>
+          <DollarRating rating={post.weekend_tips} />
+        </div>
+        <div className="each-post-rating">
+          <span className="rating-label">Clientele:</span>
+          <StarRating rating={post.clientele} />
+        </div>
       </div>
+
       <p className="each-post-comment">{post.comment}</p>
     </div>
   );
@@ -24,6 +37,8 @@ EachPost.propTypes = {
     restaurant_name: PropTypes.string.isRequired,
     title: PropTypes.string,
     clientele: PropTypes.number.isRequired,
+    weekday_tips: PropTypes.number.isRequired,
+    weekend_tips: PropTypes.number.isRequired,
     comment: PropTypes.string,
   }).isRequired,
 };
