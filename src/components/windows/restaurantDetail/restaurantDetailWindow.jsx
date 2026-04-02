384492293417d2f1effed78f7f32208e7322e668
Raymond Romero <raymond50romero@gmail.com>
2026-04-01 23:01:53 -0700

    fixing minute details on restaurant detail window

--- src/components/windows/restaurantDetail/restaurantDetailWindow.jsx
+++ src/components/windows/restaurantDetail/restaurantDetailWindow.jsx
@@ -6,20 +6,31 @@ import DollarRating from "../../../features/posts/dollarRating.jsx";
 import "./styles.css";
 
 export default function RestaurantDetailWindow() {
-  const { 
-    isDetailWindowOpen, 
-    setIsDetailWindowOpen, 
+  const {
+    isDetailWindowOpen,
+    setIsDetailWindowOpen,
     selectedRestaurantData,
     currCenter,
     currZoom,
     northEast,
-    southWest
+    southWest,
   } = useMapState();
 
   // If not open or no data, don't show
   if (!isDetailWindowOpen || !selectedRestaurantData) return null;
 
-  const { averages, restaurant_name, restaurant_address, mapbox_id, allPosts } = selectedRestaurantData;
+  const {
+    averages,
+    restaurant_name,
+    restaurant_address,
+    restaurant_place,
+    mapbox_id,
+    allPosts,
+  } = selectedRestaurantData;
+
+  const parts = restaurant_place.split(", ");
+  parts.pop(); // removes the country from place
+  const formattedPlace = parts.join(", ");
 
   const closeWindow = () => {
     setIsDetailWindowOpen(false);
@@ -32,39 +43,48 @@ export default function RestaurantDetailWindow() {
   return (
     <section className="window show-window" id="restaurant-detail-window">
       <div className="window-header">
+        <h1 className="detail-name">{restaurant_name}</h1>
         <CloseOutlined className="close-btn" onClick={closeWindow} />
       </div>
-      
-      <div className="restaurant-detail-content">
-        <h1 className="detail-name">{restaurant_name}</h1>
+      <div id="restaurant-detail-window-info">
+        <div className="overall-rating-item">
+          <StarRating rating={parseFloat(averages?.overall_average || 0)} />
+        </div>
         <p className="detail-address">{restaurant_address}</p>
-
+        <p className="detail-address">{formattedPlace}</p>
+      </div>
+      <div className="restaurant-detail-content">
         <div className="detail-ratings">
-          <h2 className="detail-section-header">Ratings</h2>
           <div className="ratings-grid">
             <div className="detail-rating-item">
-              <span>Overall:</span>
-              <StarRating rating={parseFloat(averages?.overall_average || 0)} />
-            </div>
-            <div className="detail-rating-item">
               <span>Weekday Tips:</span>
-              <DollarRating rating={parseFloat(averages?.weekday_tips_average || 0)} />
+              <DollarRating
+                rating={parseFloat(averages?.weekday_tips_average || 0)}
+              />
             </div>
             <div className="detail-rating-item">
               <span>Weekend Tips:</span>
-              <DollarRating rating={parseFloat(averages?.weekend_tips_average || 0)} />
+              <DollarRating
+                rating={parseFloat(averages?.weekend_tips_average || 0)}
+              />
             </div>
             <div className="detail-rating-item">
               <span>Management:</span>
-              <StarRating rating={parseFloat(averages?.management_average || 0)} />
+              <StarRating
+                rating={parseFloat(averages?.management_average || 0)}
+              />
             </div>
             <div className="detail-rating-item">
               <span>Environment:</span>
-              <StarRating rating={parseFloat(averages?.work_environment_average || 0)} />
+              <StarRating
+                rating={parseFloat(averages?.work_environment_average || 0)}
+              />
             </div>
             <div className="detail-rating-item">
               <span>Clientele:</span>
-              <StarRating rating={parseFloat(averages?.clientele_average || 0)} />
+              <StarRating
+                rating={parseFloat(averages?.clientele_average || 0)}
+              />
             </div>
           </div>
         </div>
